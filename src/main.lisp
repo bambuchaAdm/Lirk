6be60f507c6d3a91abcb3f4e92a83da08fcb9714
Łukasz Dubiel <bambucha14@gmail.com>
2012-03-27 21:42:57 +0200

    Przesunięcie właściwego kodu z example do src.

--- src/main.lisp
+++ src/main.lisp
@@ -13,8 +13,21 @@
 (defstruct label
   name)
 
+(defstruct asmconst
+  name)
+
+(defstruct asm-number
+  expr)
+
+(defstruct interupt-vector
+  name
+  address)
+
 (defmacro deflabel (name)
   `(defparameter ,name (make-label :name (string ',name))))
+
+(defun make-asm-label (label)
+  (format *out* "~a: ~%" (label-name label)))
 
 (defmacro print-mnemonic (name &rest args)
   `(format *out* "~t ~a ~{~a~^, ~}~%" ,name (list ,@args)))
@@ -22,8 +35,11 @@
 (defmethod ldi ((to register)(number integer))
   (print-mnemonic "ldi" (register-name to) number))
 
-(defmethod ld ((to register)(from big-register))
-  (print-mnemonic "ld" (register-name to) (big-register-name from)))
+(defmethod ldi ((to register)(number asm-number))
+  (print-mnemonic "ldi" (register-name to) (asm-number-expr number)))
+
+(defmethod ld ((to register)(from long-register))
+  (print-mnemonic "ld" (register-name to) (long-register-name from)))
 
 (defmethod sbi ((reg register)(bit integer))
   (print-mnemonic "sbi" (register-name reg)  bit))
@@ -31,8 +47,6 @@
 (defmethod cbi ((reg register)(bit integer))
   (print-mnemonic "cbi" (register-name reg) bit))
 
-(defun make-asm-label (label)
-  (format *out* "~a : ~%" (label-name label)))
 
 (defmethod jmp ((where label))
   (print-mnemonic "jmp" (label-name where)))
@@ -46,6 +60,52 @@
 (defmethod dec ((where register))
   (print-mnemonic "dec" (register-name where)))
 
+(defmethod inc ((where register))
+  (print-mnemonic "inc" (register-name where)))
+
+(defmethod ldi ((where register) (value integer))
+  (print-mnemonic "ldi" (register-name where) value))
+
+(defmethod sbis ((reg register) (bit integer))
+  (print-mnemonic "sbis" (register-name reg) bit))
+
+(defmethod cpi ((reg register) (number integer))
+  (print-mnemonic "cpi" (register-name reg) number))
+
+(defmethod breq ((where integer))
+  (print-mnemonic "breq" where))
+
+(defmethod breq ((where label))
+  (print-mnemonic "breq" (label-name where)))
+
+(defmethod brne ((where integer))
+  (print-mnemonic "brne" where))
+
+(defmethod brne ((where label))
+  (print-mnemonic "brne" (label-name where)))
+
+(defmethod brlo ((where label))
+  (print-mnemonic "brlo" (label-name where)))
+
+(defmethod brlo ((where integer))
+  (print-mnemonic "brlo" where))
+
+
+(defmethod out ((to register) (from register))
+  (print-mnemonic "out" (register-name to) (register-name from)))
+
+(defmethod clr ((which register))
+  (print-mnemonic "clr" (register-name which)))
+
+(defun cli ()
+  (print-mnemonic "cli"))
+
+(defmethod rcall ((where label))
+  (print-mnemonic "rcall" (label-name where)))
+
+(defun ret ()
+  (print-mnemonic "ret"))
+
 (defmacro main-loop (&body body)
   (let ((main-label (make-label :name "main")))
     `(progn
@@ -53,3 +113,18 @@
        ,@body
        (rjmp ,main-label))))
 
+
+(defun include (args)
+  (format *out* ".nolist~%~{.include \"~a\"~%~}.list~%" args))
+
+(defun code-segment (&optional (org nil) )
+  (format *out* ".cseg~%")
+  (if org
+      (format *out* ".org ~a~%" org)))
+
+
+(defmethod low ((arg asmconst))
+  (make-asm-number :expr (concatenate 'string "LOW(" (asmconst-name arg) ")")))
+
+(defmethod high ((arg asmconst))
+  (make-asm-number :expr (concatenate 'string "HIGH(" (asmconst-name arg) ")")))
